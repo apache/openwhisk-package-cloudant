38231226fc1ae450046f44c30a1f8b9be1a5ef03
Morgan Johnson <johnsolm@us.ibm.com>
2016-12-09 14:24:41 -0500

    adding proper package spec to persistency test cases

--- tests/src/catalog/cloudant/CloudantFeedTests.scala
+++ tests/src/catalog/cloudant/CloudantFeedTests.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package system.packages
+package catalog.cloudant
 
 import org.junit.runner.RunWith
 import org.scalatest.FlatSpec
