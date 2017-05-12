000d4f0ca276520e38c6449be457765445c6b08b
Jason Peterson <jasonpet@us.ibm.com>
2017-05-12 12:51:11 -0400

    Implement provider redundancy

--- actions/changes.js
+++ actions/changes.js
@@ -3,124 +3,69 @@ var request = require('request');
 function main(msg) {
     console.log("cloudant trigger feed: ", msg);
 
-    // lifecycleEvent is one of [ create, delete ]
-    var lifecycleEvent = (msg.lifecycleEvent || 'CREATE').trim().toUpperCase();
+    // for creation -> CREATE
+    // for deletion -> DELETE
+    var lifecycleEvent = msg.lifecycleEvent;
 
-    var namespace = process.env.__OW_NAMESPACE;
-    var triggerName = parseQName(msg.triggerName).name;
-    var triggerId = ':' + namespace + ':' + triggerName;
+    var endpoint = msg.apihost;
+    var webparams = createWebParams(msg);
 
-    // configuration parameters
-    var provider_endpoint = msg.package_endpoint;
-    var dbname = msg.dbname;
-    var user = msg.username;
-    var pass = msg.password;
-    var host = msg.host;
-    var protocol = msg.protocol || 'https';
-    var port = msg.port;
-    var maxTriggers = msg.maxTriggers;
-    var filter;
-    var query_params;
+    var url = `https://${endpoint}/api/v1/web/whisk.system/cloudantWeb/changesWebAction.http`;
 
-    if (lifecycleEvent === 'CREATE') {
-
-    	// check for parameter errors
-        if (msg.includeDoc) {
-            return Promise.reject('cloudant trigger feed: includeDoc parameter is no longer supported');
-        }
-        if (!dbname) {
-        	return Promise.reject('cloudant trigger feed: missing dbname parameter - ' + dbname);
-        }
-        if (!host) {
-        	return Promise.reject('cloudant trigger feed: missing host parameter - ' + host);
-        }
-        if (!user) {
-        	return Promise.reject('cloudant trigger feed: missing username parameter - ' + user);
-        }
-        if (!pass) {
-        	return Promise.reject('cloudant trigger feed: missing password parameter - ' + pass);
-        }
-        if (namespace === "_") {
-            return Promise.reject('You must supply a non-default namespace.');
-        }
-
-        if (msg.filter) {
-            filter = msg.filter;
-
-            if (typeof msg.query_params === 'object') {
-                query_params = msg.query_params;
-            }
-            else if (typeof msg.query_params === 'string') {
-                try {
-                    query_params = JSON.parse(msg.query_params);
-                }
-                catch (e) {
-                    return Promise.reject('The query_params parameter cannot be parsed. Ensure it is valid JSON.');
-                }
-            }
-        }
-        else if (msg.query_params) {
-            return Promise.reject('The query_params parameter is only allowed if the filter parameter is defined');
-        }
-
-        // auth key for trigger
-        var apiKey = msg.authKey;
-        var input = {};
-        input.accounturl = "https://" + host;
-        input.host = host;
-        input.port = port;
-        input.protocol = protocol;
-        input.dbname = dbname;
-        input.user = user;
-        input.pass = pass;
-        input.apikey = apiKey;
-        input.maxTriggers = maxTriggers;
-        input.filter = filter;
-        input.query_params = query_params;
-
-        return cloudantHelper(provider_endpoint, 'put', triggerId, input);
-    } else if (lifecycleEvent === 'DELETE') {
-
-        var jsonOptions = {};
-        jsonOptions.apikey = msg.authKey;
-
-        return cloudantHelper(provider_endpoint, 'delete', triggerId, jsonOptions);
-    } else {
-    	return Promise.reject('operation is neither CREATE or DELETE');
+    if (lifecycleEvent !== 'CREATE' && lifecycleEvent !== 'DELETE') {
+        return Promise.reject('lifecycleEvent must be CREATE or DELETE');
+    }
+    else {
+        var method = lifecycleEvent === 'CREATE' ? 'put' : 'delete';
+        return requestHelper(url, webparams, method);
     }
 }
 
-function cloudantHelper(endpoint, verb, name, input) {
-    var url = 'http://' + endpoint + '/cloudanttriggers/' + name;
-    var promise = new Promise(function(resolve, reject) {
+function requestHelper(url, input, method) {
+
+    return new Promise(function(resolve, reject) {
+
         request({
-            method : verb,
+            method : method,
             url : url,
-            json: input
+            json: input,
+            rejectUnauthorized: false
         }, function(error, response, body) {
-            console.log('cloudant trigger feed: done http request');
+
             if (!error && response.statusCode === 200) {
-                console.log(body);
                 resolve();
             }
             else {
                 if (response) {
-                    console.log('response code:', response.statusCode);
+                    console.log('cloudant: Error invoking whisk action:', response.statusCode, body);
                     reject(body);
-                } else {
-                    console.log('no response');
+                }
+                else {
+                    console.log('cloudant: Error invoking whisk action:', error);
                     reject(error);
                 }
             }
         });
     });
-
-    return promise;
 }
 
-function parseQName(qname) {
+function createWebParams(rawParams) {
+    var namespace = process.env.__OW_NAMESPACE;
+    var triggerName = ':' + namespace + ':' + parseQName(rawParams.triggerName, '/').name;
+
+    var webparams = Object.assign({}, rawParams);
+    delete webparams.lifecycleEvent;
+    delete webparams.bluemixServiceName;
+    delete webparams.apihost;
+
+    webparams.triggerName = triggerName;
+
+    return webparams;
+}
+
+function parseQName(qname, separator) {
     var parsed = {};
-    var delimiter = '/';
+    var delimiter = separator || ':';
     var defaultNamespace = '_';
     if (qname && qname.charAt(0) === delimiter) {
         var parts = qname.split(delimiter);
@@ -132,3 +77,7 @@ function parseQName(qname) {
     }
     return parsed;
 }
+
+exports.main = main;
+
+
---
+++ tests/src/test/scala/system/packages/CloudantFeedWebTests.scala
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2015-2016 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package system.packages
+
+import com.jayway.restassured.RestAssured
+import com.jayway.restassured.config.SSLConfig
+import com.jayway.restassured.http.ContentType
+import common.TestUtils.FORBIDDEN
+import common.{Wsk, WskProps}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+import spray.json._
+
+@RunWith(classOf[JUnitRunner])
+class CloudantFeedWebTests
+    extends FlatSpec
+    with BeforeAndAfter
+    with Matchers {
+
+    val wskprops = WskProps()
+
+    val webAction = "/whisk.system/cloudantWeb/changesWebAction"
+    val webActionURL = s"https://${wskprops.apihost}/api/v1/web${webAction}.http"
+
+    val requiredParams = JsObject(
+        "triggerName" -> JsString("/invalidNamespace/invalidTrigger"),
+        "authKey" -> JsString("DoesNotWork"),
+        "dbname" -> JsString("DoesNotExist"),
+        "host" -> JsString("bad.hostname"),
+        "username" -> JsString("username"),
+        "password" -> JsString("password")
+    )
+
+    behavior of "Cloudant changes web action"
+
+    it should "not be obtainable using the CLI" in {
+        val wsk = new Wsk()
+        implicit val wp = wskprops
+
+        wsk.action.get(webAction, FORBIDDEN)
+    }
+
+    it should "reject put of a trigger due to missing triggerName argument" in {
+        val params = JsObject(requiredParams.fields - "triggerName")
+
+        makePutCallWithExpectedResult(params, JsObject("error" -> JsString("no trigger name parameter was provided")), 400)
+    }
+
+    it should "reject put of a trigger due to missing host argument" in {
+        val params = JsObject(requiredParams.fields - "host")
+
+        makePutCallWithExpectedResult(params, JsObject("error" -> JsString("cloudant trigger feed: missing host parameter")), 400)
+    }
+
+    it should "reject put of a trigger due to missing username argument" in {
+        val params = JsObject(requiredParams.fields - "username")
+
+        makePutCallWithExpectedResult(params, JsObject("error" -> JsString("cloudant trigger feed: missing username parameter")), 400)
+    }
+
+    it should "reject put of a trigger due to missing password argument" in {
+        val params = JsObject(requiredParams.fields - "password")
+
+        makePutCallWithExpectedResult(params, JsObject("error" -> JsString("cloudant trigger feed: missing password parameter")), 400)
+    }
+
+    it should "reject put of a trigger due to missing dbname argument" in {
+        val params = JsObject(requiredParams.fields - "dbname")
+
+        makePutCallWithExpectedResult(params, JsObject("error" -> JsString("cloudant trigger feed: missing dbname parameter")), 400)
+    }
+
+    it should "reject put of a trigger when authentication fails" in {
+
+        makePutCallWithExpectedResult(requiredParams, JsObject("error" -> JsString("Trigger authentication request failed.")), 401)
+    }
+
+    it should "reject delete of a trigger due to missing triggerName argument" in {
+        val params = JsObject(requiredParams.fields - "triggerName")
+
+        makeDeleteCallWithExpectedResult(params, JsObject("error" -> JsString("no trigger name parameter was provided")), 400)
+    }
+
+    it should "reject delete of a trigger when authentication fails" in {
+        makeDeleteCallWithExpectedResult(requiredParams, JsObject("error" -> JsString("Trigger authentication request failed.")), 401)
+    }
+
+    def makePutCallWithExpectedResult(params: JsObject, expectedResult: JsObject, expectedCode: Int) = {
+        val response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .config(RestAssured.config().sslConfig(new SSLConfig().relaxedHTTPSValidation()))
+                .body(params.toString())
+                .put(webActionURL)
+        assert(response.statusCode() == expectedCode)
+        response.body.asString.parseJson.asJsObject shouldBe expectedResult
+    }
+
+    def makeDeleteCallWithExpectedResult(params: JsObject, expectedResult: JsObject, expectedCode: Int) = {
+        val response = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .config(RestAssured.config().sslConfig(new SSLConfig().relaxedHTTPSValidation()))
+                .body(params.toString())
+                .delete(webActionURL)
+        assert(response.statusCode() == expectedCode)
+        response.body.asString.parseJson.asJsObject shouldBe expectedResult
+    }
+
+}
