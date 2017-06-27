5b58923785f2818feb2523ef8426d8b3246333ae
Jason Peterson <jasonpet@us.ibm.com>
2017-06-27 14:50:21 -0400

    Add HOST_MACHINE and HOST_INDEX env vars (remove ACTIVE) (#106)

--- tests/src/test/scala/system/redundancy/CloudantRedundancyTests.scala
+++ tests/src/test/scala/system/redundancy/CloudantRedundancyTests.scala
@@ -28,8 +28,8 @@ import system.CloudantUtil
 /**
  * These tests verify that a cloudant redundancy (master/slave) configuration
  * works as expected.  They will only run properly in an environment with two
- * cloudant containers running concurrently and env var ACTIVE set to true in
- * one container and false in the other.  This test also assumes that redis and
+ * cloudant containers running concurrently and env var HOST_INDEX set to host0 in
+ * one container and host1 in the other.  This test also assumes that redis and
  * the active endpoint authorization are configured.  For the auth set the
  * ENDPOINT_AUTH env var in your containers to match the testing.auth property
  * found in your whisk.properties.  To configure redis simply set the REDIS_URL
@@ -46,7 +46,7 @@ class CloudantRedundancyTests
     val wskprops = WskProps()
     val wsk = new Wsk
     val myCloudantCreds = CloudantUtil.Credential.makeFromVCAPFile("cloudantNoSQLDB", this.getClass.getSimpleName)
-    var edgeHost = WhiskProperties.getEdgeHost()
+    var edgeHost = WhiskProperties.getEdgeHost
     val auth = WhiskProperties.getBasicAuth
     val user = auth.fst
     val password = auth.snd
@@ -182,7 +182,7 @@ class CloudantRedundancyTests
                 get(endpointURL)
         assert(response.statusCode() == 200)
         var result = response.body.asString.parseJson.asJsObject
-        JsObject(result.fields - "message") shouldBe expectedResult
+        JsObject(result.fields - "hostMachine") shouldBe expectedResult
     }
 
     override def afterAll() {
