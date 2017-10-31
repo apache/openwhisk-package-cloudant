8f2d3961e2871b1a285dd9d5866975b88483e295
Jason Peterson <jasonpet@us.ibm.com>
2017-10-31 12:40:38 -0400

    Switch to cloudant-nano (fork of nano with newer version of cloudant-follow)

--- provider/app.js
+++ provider/app.js
@@ -47,7 +47,7 @@ function createDatabase() {
     var method = 'createDatabase';
     logger.info(method, 'creating the trigger database');
 
-    var nano = require('nano')(dbProtocol + '://' + dbUsername + ':' + dbPassword + '@' + dbHost);
+    var nano = require('cloudant-nano')(dbProtocol + '://' + dbUsername + ':' + dbPassword + '@' + dbHost);
 
     if (nano !== null) {
         return new Promise(function (resolve, reject) {
