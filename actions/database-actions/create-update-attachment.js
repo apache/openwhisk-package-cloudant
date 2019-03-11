cbf47b3e899113d3f97888eb467e567bc5996247
David Grove <dgrove-oss@users.noreply.github.com>
2019-03-11 18:55:29 -0400

    fixes scancode failures exposed by switch to ASF-RELEASE.cfg (#198)

--- actions/database-actions/create-update-attachment.js
+++ actions/database-actions/create-update-attachment.js
@@ -1,3 +1,6 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more contributor
+// license agreements; and to You under the Apache License, Version 2.0.
+
 /**
  * Create and update attachment for document in Cloudant database:
  * https://docs.cloudant.com/attachments.html#create-/-update
--- actions/database-actions/delete-query-index.js
+++ actions/database-actions/delete-query-index.js
@@ -1,3 +1,6 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more contributor
+// license agreements; and to You under the Apache License, Version 2.0.
+
 /**
  * Delete a Cloudant index:
  * https://docs.cloudant.com/cloudant_query.html#deleting-an-index
